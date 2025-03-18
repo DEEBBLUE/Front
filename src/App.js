b62991d8e8948f8e73e39ad8b7205e101d0d8ef7
Lifter <Sosite.my_dick@gmail.com>
2025-03-18 16:24:41 -0400

    Add test pages in router dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main.jsx' 
 import Profile from './pages/Profile/Profile.jsx'
 import Payments from './pages/Payments/payments.jsx'
+import Games from './pages/Game/Games.jsx'
+import Funnel from './pages/Game/Funnel/Funnel.jsx'
+import Cases from './pages/Game/Cases/Cases.jsx'
+import Lotter from './pages/Game/Lotter/Lotter.jsx'
 import "./style.css"
 
 function App() {
@@ -17,6 +21,11 @@ function App() {
         <Route path="/" element={<Main callback={setAction} />}/>
         <Route path="/profile/" element={<Profile/>}/>
         <Route path="/payments" element={<Payments/>}/>
+        <Route path="/games" element={<Games/>}>
+          <Route path="/games/cases" element={<Cases/>}/>
+          <Route path="/games/funnel" element={<Funnel/>}/>
+          <Route path="/games/lottery" element={<Lotter/>}/>
+        </Route>
     </Routes>
   )
 }
