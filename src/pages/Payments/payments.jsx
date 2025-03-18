bf7d42653443952d6a7d610b9eaefb4746efaa79
Lifter <Sosite.my_dick@gmail.com>
2025-03-18 15:42:51 -0400

    Bugfix: when click on BackButton in payments page replace on profil page

--- src/pages/Payments/payments.jsx
+++ src/pages/Payments/payments.jsx
@@ -1,12 +1,19 @@
 import React,{ useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './style.css'
 
 const Payments = () => {
+  const nav = useNavigate()
   const [value,setValue] = useState(25)
+
+  window.Telegram.WebApp.MainButton.show()
+
+  window.Telegram.WebApp.BackButton.show()
+  window.Telegram.WebApp.BackButton.onClick(() => nav("/profile/",{replace: false}))
+
   const onChange = (event) => {
     setValue(event.target.value)
   }
-  window.Telegram.WebApp.MainButton.show()
   return (
     <>
       <div className="PaymentsContainer">
