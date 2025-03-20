265abd05d7c4560b308a456fddc56163e377bd60
Lifter <Sosite.my_dick@gmail.com>
2025-03-19 22:35:07 -0400

    Start anim,when user click on MainButton

--- src/pages/Game/Case/Case.jsx
+++ src/pages/Game/Case/Case.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import SpinLine from './SpinLine.jsx'
-import onClick from './spin.js'
+import onClick from './spin.js' 
+import SpinLine from './SpinLine/SpinLine.jsx'
+import AmountList from './AmountList/AmountList.jsx'
 import './style.css'
 
 const Case = () => {
-  window.Telegram.WebApp.MainButton.show()
-  window.Telegram.WebApp.MainButton.onClick(() => onClick())
+  const [anim,setAnim] = useState(false)
+  const [amountList,setAmountList] = useState([
+    {amount: " 25"},
+    {amount: "50"},
+    {amount: " 100"},
+  ])
+  if (anim){
+    window.Telegram.WebApp.MainButton.hide()
+
+    const list = document.getElementsByClassName("ListAmountContainer") 
+    const line = document.getElementsByClassName("CaseSpinLine")
+    list[0].classList.add("anim-ListAmount-remove")
+    line[0].classList.add("anim-goto-center")
+    setTimeout(() => {
+      onClick()       
+    },250)
+    setTimeout(() => {
+      const list = document.getElementsByClassName("ListAmountContainer") 
+      const line = document.getElementsByClassName("CaseSpinLine")
+      list[0].classList.remove("anim-ListAmount-remove")
+      line[0].classList.remove("anim-goto-center")   
+      console.log("her")
+      setAnim(!anim)
+    },12000 );
+  }else{
+    window.Telegram.WebApp.MainButton.show()
+    window.Telegram.WebApp.MainButton.onClick(() => setAnim(!anim))
+  }
 
   const nav = useNavigate()
   window.Telegram.WebApp.BackButton.show()
@@ -15,8 +42,9 @@ const Case = () => {
   return (
     <>
       <div className="CaseMainContainer">
+        <AmountList list={amountList}/>
         <SpinLine/>
-        <button onClick={() => onClick()}>Test button</button>
+        <button onClick={() => setAnim(!anim)}> Start </button>
       </div>
     </>
   ) 
