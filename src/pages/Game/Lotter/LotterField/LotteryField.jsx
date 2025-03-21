5c921b36aeec08ded10b6f8de3b2041a5d8d1980
Lifter <Sosite.my_dick@gmail.com>
2025-03-21 15:28:12 -0400

    Bugfix: differnce func for onClick and onTouchEnd events

--- src/pages/Game/Lotter/LotterField/LotteryField.jsx
+++ src/pages/Game/Lotter/LotterField/LotteryField.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import device from 'current-device'
 import './style.css'
 
 const LotteryField = ({index,anim}) => {
+  const onTouch = device.mobile() ? anim : () => {}
+  const onClick = device.desktop() ? anim : () => {}
   return (
-    <div key={index} id={index} className="LotteryField" onClick={() => anim(index)} onTouchEnd={() => anim(index)}>
-      <div className="LotteryPrize">
+    <div key={index} id={index} className="LotteryField" onClick={() => onClick(index)} onTouchEnd={() => onTouch(index)}>
+      <div className="LotteryPrizeHide">
       </div>
     </div>
   )
